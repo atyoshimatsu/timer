daa3a4ca28e7f5d1816996acc95f83fddbc4863b
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-08-03 15:04:48 -0700

    implemented timer2

---
+++ timer2.js
@@ -0,0 +1,21 @@
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('data', (key => {
+  if (key === 'b') {
+    process.stdout.write('\x07');
+  }
+
+  if (0 < Number(key) && Number(key) < 10) {
+    console.log(`setting timer for ${key} seconds...`);
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, Number(key) * 1000);
+  }
+
+  if (key === '\x03') {
+    console.log('Thanks for using me, ciao!');
+    process.exit();
+  }
+}));
