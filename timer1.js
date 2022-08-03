0c880d55d4bb16783b85a2fcdd9f01a324f91143
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-08-03 13:03:25 -0700

    implemetend beep timer

---
+++ timer1.js
@@ -0,0 +1,12 @@
+const argv = process.argv.slice(2);
+const beepTime = argv.map(n => Number(n)).filter(n => !Number.isNaN(n) && n >= 0);
+
+if(beepTime.length === 0) {
+  return;
+}
+
+for (const time of beepTime) {
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, time * 1000);
+}
