e8f8c59bfceb96084d1e2e28963fce90ba84fd3e
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-08-07 16:31:28 +0000

    Wrap the main process as a function

--- timer1.js
+++ timer1.js
@@ -1,12 +1,16 @@
 const argv = process.argv.slice(2);
 const beepTime = argv.map(n => Number(n)).filter(n => !Number.isNaN(n) && n >= 0);
 
-if (beepTime.length === 0) {
-  return;
-}
+const beepTimer = (beepTime) => {
+  if (beepTime.length === 0) {
+    return;
+  }
 
-for (const time of beepTime) {
-  setTimeout(() => {
-    process.stdout.write('\x07');
-  }, time * 1000);
-}
+  for (const time of beepTime) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, time * 1000);
+  }
+};
+
+beepTimer(beepTime);
